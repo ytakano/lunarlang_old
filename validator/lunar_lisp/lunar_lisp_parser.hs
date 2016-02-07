91e762a93b6a71a1cddcfbe4a05721b11d4ab43e
Yuuki Takano <ytakanoster@gmail.com>
2016-02-08 05:56:54 +0900

    fix BNF of lunar lisp

--- validator/lunar_lisp/lunar_lisp_parser.hs
+++ validator/lunar_lisp/lunar_lisp_parser.hs
@@ -8,32 +8,41 @@ import           Text.Parsec            ((<?>))
 import qualified Text.Parsec            as Parsec
 
 {- BNF of Lunar Lisp
-exprs  ::= expr exprs | ε
-expr   ::= '(' sp term sp args ')' | '(' sp expr sp args sp ')'
+exprs   ::= expr exprs | ε
+expr    ::= '(' sp term sp args ')' | '(' sp expr sp args sp ')'
 
-list   ::= '\'(' sp args sp ')'
+list    ::= '\'(' sp args sp ')'
 
-args   ::= arg sp args | ε
-arg    ::= expr | list | num | term
+args    ::= arg sp args | ε
+arg     ::= expr | list | num | term
 
-term   ::= nonum chars
-chars  ::= utf8 chars | ε
+term    ::= nonum chars
+chars   ::= utf8 chars | ε
 
-num    ::= oct | hex | bin | digits | digits '.' digits
-digits ::= digit digits | ε
-hex    ::= 'x' digits | 'X' digits
-bin    ::= 'b' digits | 'B' digits
-oct    ::= '0' digits
+num     ::= hex | oct | bin | float | natural
+float   ::= natural '.' digits
+natural ::= nonzero digits
+digits  ::= digit digits | ε
+hex     ::= '0x' hexnunm hexs
+hexs    ::= hexnum hexs | ε
+oct     ::= '0o' octnum octs
+octs    ::= octnum octs | ε
+bin     ::= '0b' binum bins
+bins    ::= binnum bins | ε
 
-reserv ::= '(' | ')' | '.' | '\'' | '"' | ';'
-sp     ::= space sp | ε
-space  ::= ' ' | '\r' | '\n' | '\t'
-alpha  ::= [a-zA-Z]
-digit  ::= [0-9]
-op     ::= '+' | '/' | '~' | '!' | '@' | '#' | '$' | '%' | ':' |
-           '^' | '^' | '*' | '-' | '=' | '_' | '<' | '>' | '|'
-utf8   ::= alpha | digit | op | multibyte character
-nonum  ::= alpha | op | multibyte character
+reserv  ::= '(' | ')' | '.' | '\'' | '"' | ';'
+sp      ::= space sp | ε
+space   ::= ' ' | '\r' | '\n' | '\t'
+alpha   ::= [a-zA-Z]
+digit   ::= [0-9]
+nonzero ::= [1-9]
+binnum  ::= '0' | '1'
+octnum  ::= [0-7]
+hexnum  ::= [0-9a-fA-F] 
+op      ::= '+' | '/' | '~' | '!' | '@' | '#' | '$' | '%' | ':' |
+            '^' | '^' | '*' | '-' | '=' | '_' | '<' | '>' | '|'
+utf8    ::= alpha | digit | op | multibyte character
+nonum   ::= alpha | op | multibyte character
 -}
 
 parse :: Parsec.Stream s Identity t => Parsec.Parsec s () a -> s -> Either Parsec.ParseError a
