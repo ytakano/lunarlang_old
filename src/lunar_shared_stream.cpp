59348352bce4846ce48b80120ed21b855e17030c
Yuuki Takano <ytakanoster@gmail.com>
2016-02-20 20:28:22 +0900

    implement shared stream

--- src/lunar_shared_stream.cpp
+++ src/lunar_shared_stream.cpp
@@ -1,1 +1,64 @@
 #include "lunar_shared_stream.hpp"
+
+namespace lunar {
+
+extern "C" {
+
+void
+make_shared_stream(shared_stream *p, stream_t srm)
+{
+    p->stream = srm;
+    p->flag   = shared_stream::READ | shared_stream::WRITE;
+    
+    p->shared_data = new shared_stream::shared_data_t;
+    
+    p->shared_data->flag_shared = 0;
+    p->shared_data->refcnt = 1;
+}
+
+// if shared_stream will be transfered to another thread,
+// is_mt must be set true
+void
+make_shared_write_only_stream(shared_stream *dst,
+                              shared_stream *src, bool is_mt)
+{
+    if (src->shared_data->flag_shared & shared_stream::MT) {
+        spin_lock_acquire lock(src->shared_data->lock);
+        
+        dst->stream      = src->stream;
+        dst->flag        = shared_stream::WRITE;
+        dst->shared_data = src->shared_data;
+        dst->shared_data->refcnt++;
+    } else {
+        dst->stream      = src->stream;
+        dst->flag        = shared_stream::WRITE;
+        dst->shared_data = src->shared_data;
+        dst->shared_data->refcnt++;
+        
+        if (is_mt) {
+            dst->shared_data->flag_shared |= shared_stream::MT;
+        }
+    }
+}
+
+bool
+deref_shared_stream(shared_stream *ptr)
+{
+    if (ptr->shared_data->flag_shared & shared_stream::MT) {
+        spin_lock_acquire lock(ptr->shared_data->lock);
+        
+        ptr->shared_data->refcnt--;
+        if (ptr->shared_data->refcnt == 0)
+            return true;
+    } else {
+        ptr->shared_data->refcnt--;
+        if (ptr->shared_data->refcnt == 0)
+            return true;
+    }
+
+    return false;
+}
+
+} // extern "C"
+
+} // namespace lunar
--- src/lunar_shared_stream.hpp
+++ src/lunar_shared_stream.hpp
@@ -2,26 +2,41 @@
 #define LUNAR_SHARED_STREAM_HPP
 
 #include "lunar_common.hpp"
+#include "lunar_spin_lock.hpp"
 
 namespace lunar {
 
+// container for stream
 // every stream is multiple-writers and single-reader
+
+union stream_t {
+    int   fd;
+    void *ptr;
+};
 
 struct shared_stream {
     static const uint32_t READ         = 0x0001;
     static const uint32_t WRITE        = 0x0002;
-    static const uint32_t CLOSED_WRITE = 0x0004;
-    static const uint32_t MT           = 0x0008;
+    static const uint32_t CLOSED_READ  = 0x0004;
+    static const uint32_t CLOSED_WRITE = 0x0008;
+    static const uint32_t MT           = 0x00F0;
 
-    union {
-        int   fd;
-        void *ptr;
-    } m_data;
-
-    uint16_t  m_flag; // READ, WRITE
-    uint16_t *m_flag_shared; // CLOSED_WRITE, MT
-    uint32_t *m_refcnt;
+    stream_t  stream;
+    uint32_t  flag;  // READ, WRITE
+    
+    struct shared_data_t {
+        uint32_t  flag_shared; // CLOSED_READ, CLOSED_WRITE, MT
+        uint32_t  refcnt;
+        spin_lock lock;
+    } *shared_data;
 };
+
+extern "C" {
+    void make_shared_stream(shared_stream *p, stream_t srm);
+    void make_shared_write_only_stream(shared_stream *dst,
+                                       shared_stream *src, bool is_mt);
+    bool deref_shared_stream(shared_stream *ptr);
+}
 
 }
 
