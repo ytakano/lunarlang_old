7ee80a3f39169e561f9cd208d972c7684cd0b3be
Yuuki Takano <ytakanoster@gmail.com>
2016-03-04 16:14:10 +0900

    rename lunar lisp to lunar IR

---
+++ src/lunar_ir.hpp
@@ -0,0 +1,72 @@
+#ifndef LUNAR_IR_HPP
+#define LUNAR_IR_HPP
+
+#include "lunar_common.hpp"
+#include "lunar_string.hpp"
+
+#include <vector>
+
+namespace lunar {
+
+class lunar_ir_ast {
+public:
+    lunar_ir_ast() { }
+    virtual ~lunar_ir_ast() { }
+};
+
+class lunar_ir_type_ast : public lunar_ir_ast {
+public:
+    enum MEMTYPE {
+        UNIQUE,
+        SHARED,
+        REFERENCE,
+        IMMOVABLE,
+    };
+    
+    lunar_ir_type_ast(MEMTYPE memtype, const std::u32string &tname) : m_memtype(memtype), m_typename(tname) { }
+    ~lunar_ir_type_ast() { }
+    
+    MEMTYPE get_memtype() { return m_memtype; }
+    const std::u32string & get_typename() { return m_typename; }
+
+
+private:
+    MEMTYPE m_memtype;
+    std::u32string m_typename;
+};
+
+class lunar_ir_variable_ast : public lunar_ir_ast {
+public:
+    lunar_ir_variable_ast(std::unique_ptr<lunar_ir_type_ast> type, const std::u32string &name)
+        : m_type(std::move(type)), m_name(name) { }
+    virtual ~lunar_ir_variable_ast() { }
+    
+    const lunar_ir_type_ast & get_type() const { return *m_type; }
+    const std::u32string & get_name() const { return m_name; }
+
+private:
+    std::unique_ptr<lunar_ir_type_ast> m_type;
+    std::u32string m_name;
+};
+
+class lunar_ir_data_ast : public lunar_ir_ast {
+public:
+    lunar_ir_data_ast() { }
+    virtual ~lunar_ir_data_ast() { }
+    
+    void push(std::unique_ptr<lunar_ir_variable_ast> var) { m_vars.push_back(std::move(var)); }
+
+private:
+    std::vector<std::unique_ptr<lunar_ir_variable_ast>> m_vars;
+};
+
+class lunar_ir_list_ast : public lunar_ir_ast {
+public:
+
+private:
+
+};
+
+}
+
+#endif // LUNAR_IR_HPP
