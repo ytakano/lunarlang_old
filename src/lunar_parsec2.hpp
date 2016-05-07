d10c046de33e9abb6cc56604e7acf7eae8d02ee4
Yuuki Takano <ytakanoster@gmail.com>
2016-05-07 22:16:46 +0900

    writing alternate parser combinator in C++

--- src/lunar_parsec2.hpp
+++ src/lunar_parsec2.hpp
@@ -30,7 +30,7 @@ public:
         T    m_char;
         bool m_is_result;
         
-        bool operator bool() const { return m_is_result; }
+        operator bool() const { return m_is_result; }
     };
     
     class parser_char {
@@ -56,7 +56,7 @@ public:
     
     class parser_satisfy {
     public:
-        parser_satisfy(parsec &p, std::function<char_t(T)> func) : m_parsec(p), m_func(func) { }
+        parser_satisfy(parsec2 &p, std::function<char_t(T)> func) : m_parsec(p), m_func(func) { }
         virtual ~parser_satisfy() { }
 
         char_t operator() () {
@@ -120,7 +120,7 @@ public:
 
     class parser_try {
     public:
-        parser_try(parsec &p) : m_parsec(p) {
+        parser_try(parsec2 &p) : m_parsec(p) {
             m_col    = m_parsec.m_col;
             m_line   = m_parsec.m_line;
             m_num    = m_parsec.m_num;
@@ -129,21 +129,22 @@ public:
             
             m_parsec.m_is_try = true;
         }
+
         virtual ~parser_try() {
             if (m_parsec.m_result) {
                 if (! m_is_try && ! m_parsec.m_is_look_ahead) {
-                   	auto n = m_parsec.m_num - num;
-                    m_parsec.m_bytes.restore_tmp_pos(pos);
+                   	auto n = m_parsec.m_num - m_num;
+                    m_parsec.m_bytes.restore_tmp_pos(m_pos);
                     m_parsec.m_bytes.consume(n);
                 }
                 
-                parser::m_parsec.m_is_try = is_try;
+                m_parsec.m_is_try = m_is_try;
             } else {
                 m_parsec.m_col    = m_col;
                 m_parsec.m_line   = m_line;
                 m_parsec.m_num    = m_num;
                 m_parsec.m_is_try = m_is_try;
-                m_parsec.m_bytes.restore_tmp_pos(pos);
+                m_parsec.m_bytes.restore_tmp_pos(m_pos);
             }
         }
     
@@ -154,20 +155,94 @@ public:
         bool      m_is_try;
     };
     
+    class parser_look_ahead {
+    public:
+        parser_look_ahead(parsec2 &p) : m_parsec(p) {
+            m_col  = m_parsec.m_col;
+            m_line = m_parsec.m_line;
+            m_num  = m_parsec.m_num;
+            m_pos  = m_parsec.m_bytes.get_tmp_pos();
+            m_is_look_ahead = m_parsec.m_is_look_ahead;
+            
+            m_parsec.m_is_look_ahead = true;
+        }
+
+        virtual ~parser_look_ahead() {
+            m_parsec.m_col  = m_col;
+            m_parsec.m_line = m_line;
+            m_parsec.m_num  = m_num;
+            m_parsec.m_is_look_ahead = m_is_look_ahead;
+            m_parsec.m_bytes.restore_tmp_pos(m_pos);
+         }
+    
+    private:
+        parsec2  &m_parsec;
+        uint64_t  m_col, m_line, m_num;
+        point2u64 m_pos;
+        bool      m_is_look_ahead;
+    };
+    
+    class parser_string {
+    public:
+        parser_string(parsec2 &p, ptr_string_t str)
+            : m_parsec(p),
+              m_str_std(std::move(str)),
+              m_str_arr(m_str_std.c_str()),
+              m_len(m_str_std.size()) { }
+
+        parser_string(parsec2 &p, const T *str) : m_parsec(p), m_str_arr(str) {
+            m_len = 0;
+            while (*str != (T)0) m_len++;
+        }
+
+        virtual ~parser_string() { }
+        
+        ptr_string_t operator() () {
+            ptr_string_t ret = llvm::make_unique<string_t>();
+            
+            for (uint64_t i = 0; i < m_len; i++) {
+                auto c = m_parsec.character(m_str_arr[i]);
+                if (c) {
+                    ret->push_back(c.m_char);
+                } else {
+                    return nullptr;
+                }
+            }
+            
+            return std::move(ret);
+        }
+        
+    private:
+        parsec2     &m_parsec;
+        ptr_string_t m_str_std;
+        const T     *m_str_arr;
+        uint64_t     m_len;
+    };
+    
     template <typename RET>
     class parser_many {
     public:
-        parser_many(parsec &p, std::function<char_t(T)> func) : m_parsec(p), m_func(func) { }
+        parser_many(parsec2 &p, std::function<RET()> func) : m_parsec(p), m_func(func) { }
         virtual ~parser_many() { }
         
         std::unique_ptr<std::vector<RET>> operator() () {
             auto vals = llvm::make_unique<std::vector<RET>>();
+            
+            for (;;) {
+                RET ret = m_func();
+                if (ret) {
+                    vals.push_back(ret);
+                } else {
+                    break;
+                }
+            }
             
             return vals;
         }
     
     private:
         parsec2 &m_parsec;
+        std::function<RET()> m_func;
     };
     
     parsec2(shared_stream s)
@@ -176,7 +251,6 @@ public:
           m_col(1),
           m_line(1),
           m_num(0),
-          m_is_skip(false),
           m_is_look_ahead(false),
           m_is_try(false) { }
     virtual ~parsec2() { }
@@ -186,6 +260,20 @@ public:
         m_err.result = result;
         m_err.line   = line;
         m_err.col    = col;
+    }
+    
+    parser_satisfy satisfy(std::function<char_t(T)> f)
+    {
+        return parser_satisfy(*this, f);
+    }
+    
+    parser_satisfy character(T c) {
+        return parser_satisfy(*this, parser_char(c));
+    }
+    
+    template <typename RET>
+    parser_many<RET> many(std::function<RET()> func) {
+        return parser_many<RET>(*this, func);
     }
 
 private:
--- tests/json/json_cpp.cpp
+++ tests/json/json_cpp.cpp
@@ -1,4 +1,4 @@
-#include <lunar_parsec.hpp>
+#include <lunar_parsec2.hpp>
 
 class json_val {
 public:
@@ -22,27 +22,31 @@ public:
     std::u32string m_str;
 };
 
-std::unique_ptr<json_double>
-parse_number(lunar::parsec<char32_t> &ps)
-{
-    return nullptr;
-}
-
-std::unique_ptr<json_string>
-parse_string(lunar::parsec<char32_t> &ps)
-{
-    auto c1 = ps.character('"');
-    
-    if (! c1()) {
-        return nullptr;
-    }
-    
-    return nullptr;
-}
-
 int
 main(int argc, char **argv)
 {
+    lunar::init_fiber();
+    
+    auto func = [] () {
+        auto rs = new lunar::shared_stream;
+        auto ws = new lunar::shared_stream;
+        lunar::make_ptr_stream(rs, ws, 32);
+        
+        lunar::parsec2<char32_t> parsec(*rs);
+
+        auto text = new std::u32string(U"12345.67abc");
+        
+        push_string(ws, text);
+        push_eof_string(ws);
+        
+        deref_ptr_stream(rs);
+        deref_ptr_stream(ws);
+        delete rs;
+        delete ws;
+    };
+    
+    lunar::spawn_fiber(func);
+    lunar::run_fiber();
     
     return 0;
 }
