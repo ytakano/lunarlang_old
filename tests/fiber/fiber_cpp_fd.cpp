17bd75d5527853b8b109632ac31614f22987dcab
Yuuki Takano <ytakanoster@gmail.com>
2016-05-17 00:45:42 +0900

    add ifdef for kqueue

--- tests/fiber/fiber_cpp_fd.cpp
+++ tests/fiber/fiber_cpp_fd.cpp
@@ -7,10 +7,13 @@ func(void *arg)
 {
     printf("> ");
     fflush(stdout);
+
+#ifdef KQUEUE
     struct kevent kev;
     EV_SET(&kev, STDIN_FILENO, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
     lunar::select_fiber(&kev, 1, nullptr, 0, false, 10000);
-    
+#endif // KQUEUE
+
     if (lunar::is_timeout_fiber()) {
         printf("\ntimeout!\n");
     } else {
