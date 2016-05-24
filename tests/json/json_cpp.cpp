44dfc9061dd48679d3a8733a3f2e49b248051b7f
Yuuki Takano <ytakanoster@gmail.com>
2016-05-24 16:16:10 +0900

    fix bugs. parse number in C++

--- tests/json/json_cpp.cpp
+++ tests/json/json_cpp.cpp
@@ -1,5 +1,6 @@
 #include <lunar_parsec.hpp>
 #include <iostream>
+#include <stdlib.h>
 
 class json_val {
 public:
@@ -9,7 +10,7 @@ public:
 
 class json_double : public json_val {
 public:
-    json_double() { }
+    json_double(double num) : m_num(num) { }
     virtual ~json_double() { }
     
     double m_num;
@@ -23,23 +24,126 @@ public:
     std::string m_str;
 };
 
+std::string parse_many_digit(lunar::parsec<char> &ps);
+
+std::string
+parse_frac(lunar::parsec<char> &ps)
+{
+    std::string s;
+
+    auto dot = ps.character('.')();
+    
+    if (! dot)
+        return s;
+    
+    auto c = ps.parse_digit()();
+    
+    if (! c)
+        return s;
+    
+    s  = ".";
+    s += c.m_char;
+    s += parse_many_digit(ps);
+    
+    return s;
+}
+
+std::string
+parse_many_digit(lunar::parsec<char> &ps)
+{
+    std::string s;
+
+    // many digit
+    for (;;) {
+        lunar::parsec<char>::parser_try t(ps);
+        auto c = ps.parse_digit()();
+
+        if (! c)
+            break;
+
+        s += c.m_char;
+    }
+    
+    ps.set_is_result(true);
+    
+    return s;
+}
+
+std::string
+parse_digit1_9(lunar::parsec<char> &ps)
+{
+    auto one2nine = [](char c) -> lunar::parsec<char>::char_t {
+        return {c, '1' <= c && c <= '9'};
+    };
+    
+    std::string s;
+    
+    auto c = ps.satisfy(one2nine)();
+    if (! c)
+        return s;
+
+    s += c.m_char;
+    s += parse_many_digit(ps);
+
+    return s;
+}
+
+// number          = [ minus ] int [ frac ] [ exp ]
+json_double
+parse_number(lunar::parsec<char> &ps)
+{
+    std::string s;
+
+    {
+        lunar::parsec<char>::parser_try t(ps);
+        auto sign = ps.character('-')();
+        
+        if (sign)
+            s += "-";
+    }
+    
+    {
+        lunar::parsec<char>::parser_try t(ps);
+        auto zero = ps.character('0')();
+        
+        if (zero) {
+            return json_double(0);
+        }
+    }
+    
+    s += parse_digit1_9(ps);
+    
+    std::cout << s << std::endl;
+    
+    if (! ps.get_is_result()) {
+        return json_double(0);
+    }
+    
+    {
+        lunar::parsec<char>::parser_try t(ps);
+        auto frac = parse_frac(ps);
+        
+        if (ps.get_is_result())
+            s += frac;
+    }
+    
+    ps.set_is_result(true);
+    
+    return json_double(strtod(s.c_str(), nullptr));
+}
+
 void
 parser_json(void *arg)
 {
     auto rs = (lunar::shared_stream*)arg;
     
-    for (;;) {
-        std::string *str;
-        auto ret = lunar::pop_ptr(rs, (void**)&str);
-        if (ret == lunar::STRM_SUCCESS) {
-            std::cout << "input!: " << *str
-                      << "\n> " << std::flush;
-            delete str;
-        } else if (ret == lunar::STRM_CLOSED) {
-            break;
-        } else {
-            std::cout << "error!" << std::endl;
-        }
+    lunar::parsec<char> ps(rs);
+    
+    json_double d = parse_number(ps);
+    if (ps.get_is_result()) {
+        std::cout << "input = " << d.m_num << "\n> " << std::flush;
+    } else {
+        std::cout << "failed to parse\n> " << std::flush;
     }
     
     lunar::deref_ptr_stream(rs);
