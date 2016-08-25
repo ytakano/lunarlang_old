6835cc65028a788a2dd4d9a5ebdad9a3c85484d8
Yuuki Takano <ytakanoster@gmail.com>
2016-08-26 06:39:13 +0900

    add hopscotch hashing

--- tests/container/bench_container.cpp
+++ tests/container/bench_container.cpp
@@ -1,5 +1,6 @@
 #include "../../src/lunar_hash.hpp"
 #include "../../src/lunar_slab_allocator.hpp"
+#include "hopscotch.hpp"
 
 #include <unordered_set>
 
@@ -197,6 +198,51 @@ bench_unordered_slab()
     return n;
 }
 
+uint64_t
+bench_hopscotch()
+{
+    nanahan::Map<uint64_t, uint64_t, std::hash<uint64_t>, std::equal_to<uint64_t>, lunar::slab_allocator<std::pair<const uint64_t, uint64_t>>> hs;
+    timeval  tm0, tm1;
+    uint64_t n = 0;
+
+    gettimeofday(&tm0, nullptr);
+
+    for (int j = 0; j < NTRIAL; j++) {
+        for (int i = 0; i < NUM; i++) {
+            hs.insert({i, i});
+        }
+    }
+
+    gettimeofday(&tm1, nullptr);
+    printf("hopscotch<slab>: insertion:\t\t\t%lf[ops/s]\n", NUM * NTRIAL / diff_tm(tm0, tm1));
+    gettimeofday(&tm0, nullptr);
+
+    for (int j = 0; j < NTRIAL; j++) {
+        for (int i = 0; i < NUM; i++) {
+            hs.erase(hs.find(i));
+            hs.insert({i, i});
+        }
+    }
+
+    gettimeofday(&tm1, nullptr);
+    printf("hopscotch<slab>: insertion & deletion:\t\t%lf[ops/s]\n", NUM * NTRIAL / diff_tm(tm0, tm1));
+
+
+    gettimeofday(&tm0, nullptr);
+
+    for (int j = 0; j < NTRIAL; j++) {
+        for (int i = 0; i < NUM; i++) {
+            if (hs.find(i) != hs.end())
+                n += i;
+        }
+    }
+
+    gettimeofday(&tm1, nullptr);
+    printf("hopscotch<slab>: lookup:\t\t\t%lf[ops/s]\n", NUM * NTRIAL / diff_tm(tm0, tm1));
+
+    return n;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -204,6 +250,7 @@ main(int argc, char *argv[])
     printf("lunar::hash_map:     n = %llu\n", bench_lunar_hash_map());
     printf("unordered_set:       n = %llu\n", bench_unordered());
     printf("unordered_set<slab>: n = %llu\n", bench_unordered_slab());
+    printf("hopscotch<slab>:     n = %llu\n", bench_hopscotch());
 
     return 0;
 }
