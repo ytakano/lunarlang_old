49884db13fdc6cd28c95e705d5225a50830962d4
Yuuki Takano <ytakanoster@gmail.com>
2017-03-10 19:44:43 +0900

    use 'ifdef __linux__'

--- src/lunar_green_thread.cpp
+++ src/lunar_green_thread.cpp
@@ -682,7 +682,7 @@ green_thread::spawn(void (*func)(void*), void *arg, int stack_size)
 
     void *addr;
 
-#ifdef linux
+#ifdef __linux__
     addr = mmap(NULL, stack_size, PROT_READ | PROT_WRITE, MAP_GROWSDOWN | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
     if (addr == MAP_FAILED) {
         PRINTERR("failed mmap!: %s", strerror(errno));
@@ -713,7 +713,7 @@ green_thread::spawn(void (*func)(void*), void *arg, int stack_size)
         PRINTERR("failed mprotect!: %s", strerror(errno));
         exit(-1);
     }
-#endif // linux
+#endif // __linux__
 
     m_suspend.push_back(ctx.get());
     m_id2context[m_count] = std::move(ctx);
@@ -793,11 +793,11 @@ green_thread::schedule()
             if (state & context::READY) {
                 if (ctx) {
                     if (sigsetjmp(ctx->m_jmp_buf, 0) == 0) {
-#ifdef linux
+#ifdef __linux__
                         auto p = &m_running->m_stack[-4];
 #else
                         auto p = &m_running->m_stack[m_running->m_stack_size - 4];
-#endif // linux
+#endif // __linux__
                         asm (
                             "movq %0, %%rsp;" // set stack pointer
                             "movq %0, %%rbp;" // set frame pointer
@@ -812,11 +812,11 @@ green_thread::schedule()
                         return;
                     }
                 } else {
-#ifdef linux
+#ifdef __linux__
                     auto p = &m_running->m_stack[-4];
 #else
                     auto p = &m_running->m_stack[m_running->m_stack_size - 4];
-#endif // linux
+#endif // __linux__
                     asm (
                         "movq %0, %%rsp;" // set stack pointer
                         "movq %0, %%rbp;" // set frame pointer
@@ -1193,7 +1193,7 @@ green_thread::remove_stopped()
 {
     int pagesize = sysconf(_SC_PAGE_SIZE);
     for (auto ctx: m_stop) {
-#ifdef linux
+#ifdef __linux__
         munmap(ctx->m_stack);
 #else
         if (mprotect(&ctx->m_stack[0], pagesize, PROT_READ | PROT_WRITE) < 0) {
@@ -1201,7 +1201,7 @@ green_thread::remove_stopped()
             exit(-1);
         }
         free(ctx->m_stack);
-#endif // linux
+#endif // __linux__
         m_id2context.erase(ctx->m_id);
     }
 
